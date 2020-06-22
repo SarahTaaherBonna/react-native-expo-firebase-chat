27ae865bd447c04bf19166e82c650925a4ececf5
SarahTaaherBonna <sarahtb25@gmail.com>
2020-06-22 13:45:12 +0800

    Fixed previous error, facing issues with Fonts in @expo

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  resolver: {
-    sourceExts: ["jsx", "js", "ts", "tsx", "json", "wasm"], //add here
-  },
-};
+// module.exports = {
+//   resolver: {
+//     sourceExts: ["jsx", "js", "ts", "tsx", "json", "wasm"], //add here
+//   },
+// };
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    resolver: {
+      assetExts: [assetExts, "txt", "xml", "png", "jpg"],
+      sourceExts: [...sourceExts, "txt", "xml", "png", "jpg"],
+    },
+  };
+})();
